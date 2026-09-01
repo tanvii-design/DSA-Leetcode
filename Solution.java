57604cbd56c3faa3226e8ba53ec5112f18879077
Tanvi Chauhan <tanviic33@gmail.com>
2026-09-01 22:19:45 +0530

    Add files via upload

---
+++ Solution.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+class Solution {
+    public int reverse(int x) {
+        long reverse=0;
+        while (x != 0) {
+            int reminder=x%10;
+            reverse=reverse*10+reminder;
+            x=x/10;
+        }
+        if (reverse > Integer.MAX_VALUE || reverse < Integer.MIN_VALUE) {
+            return 0;   // too big to fit, so return 0
+        }
+        return (int) reverse;
+    }
+}
+class Main10{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a number");
+        int n=sc.nextInt();
+        Solution obj=new Solution();
+        int reverse= obj.reverse(n);
+        System.out.println("reverse="+reverse);
+    }
+}
